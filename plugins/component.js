d4c089c61b59cf4f079a485029c59d88206eae7b
Sergey Antonov <flyker777@yandex.ru>
2019-11-17 22:03:36 +0300

    update router and language

--- plugins/component.js
+++ plugins/component.js
@@ -4,9 +4,11 @@ import Teaser from '~/components/Teaser.vue'
 import Grid from '~/components/Grid.vue'
 import Feature from '~/components/Feature.vue'
 import Slide from '../components/Slide.vue'
+import SitemapItem from '~/components/SitemapItem.vue'
 
 Vue.component('page', Page)
 Vue.component('teaser', Teaser)
 Vue.component('grid', Grid)
 Vue.component('feature', Feature)
 Vue.component('slide', Slide)
+Vue.component('sitemap-item', SitemapItem)
